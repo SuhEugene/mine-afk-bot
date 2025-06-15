4bf3fcf1fbf743a8f4006b687a402d6f66587c96
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2025-06-15 16:40:46 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const mineflayer = require('mineflayer')
+
+// const test = process.argv[2] ? process.argv[2] == "test" : true;
+const test = false;
+console.log("Joining", test ? "test" : "spworlds");
+
+const bot = mineflayer.createBot({
+	host: test ? "51.83.49.23" : "sp.spworlds.ru",
+	port: test ? 25629 : null,
+	auth: "microsoft"
+});
+
+bot.once("spawn", ()=>console.log("Logged in!"));
+
+bot.on('kicked', (reason, ...args) => {
+	console.log("Kicked", reason, ...args);
+	if (reason?.text == "Connection throttled! Please wait before reconnecting.") {
+		console.log("Waiting 1 min");
+		setTimeout(()=>{process.exit(1)}, 60000);
+		return;
+	}
+	console.log("Waiting 3 mins");
+	setTimeout(()=>{
+		process.exit(1);
+	}, 180000);
+});
+bot.on('error', console.error);
+/*bot.on('chat', (username, message) => {
+	console.log(`<${username}> ${message}`);
+});*/
